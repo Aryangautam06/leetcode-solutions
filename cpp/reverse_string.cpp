b56af8f589109607750dfd9ecf41a65af31e7f82
Aryan Gautam <aryan424dx@gmail.com>
2026-08-27 21:42:54 +0530

    LeetCode: update cpp/reverse_string.cpp

---
+++ cpp/reverse_string.cpp
@@ -0,0 +1,26 @@
+// ======================================
+// LeetCode Problem: reverse string
+// Language: cpp
+// Link: https://leetcode.com/problems/reverse-string/
+// Synced by: LinkCode
+// Date: 27/08/2026, 21:42:53
+// ======================================
+
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        //while loop lagana hai
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+
+        }
+        }
+};
