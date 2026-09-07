d06f4b0506978ef7f8e819707efae030c014fcbc
Aryan Gautam <aryan424dx@gmail.com>
2026-09-07 16:37:09 +0530

    LeetCode: update cpp/linked_list_cycle.cpp

---
+++ cpp/linked_list_cycle.cpp
@@ -0,0 +1,32 @@
+// ======================================
+// LeetCode Problem: linked list cycle
+// Language: cpp
+// Link: https://leetcode.com/problems/linked-list-cycle/
+// Synced by: LinkCode
+// Date: 07/09/2026, 16:37:07
+// ======================================
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast=head;
+        ListNode *slow=head;
+        while(fast != NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
