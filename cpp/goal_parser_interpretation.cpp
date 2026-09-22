b467df40957af5d7b54cc9c516a2dea2bd518677
Aryan Gautam <aryan424dx@gmail.com>
2026-09-22 23:05:44 +0530

    LeetCode: update cpp/goal_parser_interpretation.cpp

---
+++ cpp/goal_parser_interpretation.cpp
@@ -0,0 +1,32 @@
+// ======================================
+// LeetCode Problem: goal parser interpretation
+// Language: cpp
+// Link: https://leetcode.com/problems/goal-parser-interpretation/
+// Synced by: LinkCode
+// Date: 22/09/2026, 23:05:44
+// ======================================
+
+
+class Solution {
+public:
+    string interpret(string command) {
+        string ans;
+        int n=command.size();
+        int i=0;
+        while(i<n){
+            if(command[i]=='(' &&command[i+1]=='a'){
+                ans+="al";
+                i+=4;
+            }
+            else if(command[i]=='('&& command[i+1]==')'){
+                ans+="o";
+                i+=2;
+            }
+            else{
+                ans+="G";
+                i++;
+            }
+        }
+        return ans;
+    }
+};
